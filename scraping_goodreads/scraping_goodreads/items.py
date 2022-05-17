58c37489788486d9495ef9e9c7e91f81f9f851fd
asmakrl <90398895+asmakrl@users.noreply.github.com>
2022-05-18 00:20:20 +0100

    done...But still working on it

--- scraping_goodreads/scraping_goodreads/items.py
+++ scraping_goodreads/scraping_goodreads/items.py
@@ -8,7 +8,7 @@ import scrapy
 
 class ScrapingGoodreadsItem(scrapy.Item):
     # define the fields for your item here like:
-    titles = scrapy.Field()
-    authors = scrapy.Field()
-    tags = scrapy.Field()
-    
+    #titles = scrapy.Field()
+    #authors = scrapy.Field()
+    #description = scrapy.Field()
+    reviews = scrapy.Field()
--- scraping_goodreads/scraping_goodreads/spiders/spider_quotes.py
+++ scraping_goodreads/scraping_goodreads/spiders/spider_quotes.py
@@ -5,27 +5,44 @@ from ..items import ScrapingGoodreadsItem
 class QuoteSpider( scrapy.Spider ):
 
   name = 'quotes'
-  start_urls = [
 
-      'http://quotes.toscrape.com/'
-  ]
+  page_number = 2
+
+  start_urls = [
     
+    'https://www.goodreads.com/book/show/10210.Jane_Eyre?page=1'
+    
+    ]
+
+     
   
   # First parse method
   def parse( self, response ):
-
+    
     items = ScrapingGoodreadsItem()
 
-    div_quotes= response.css('div.quote')
+    #div_title= response.css('div.leftContainer')
     
-    for quotes in div_quotes:
+    #for divs in div_title:
 
-      titles = quotes.css('span.text::text').extract()
-      authors = quotes.css('small.author::text').extract()
-      tags = quotes.css('a.tag::text').extract()
+   # title = response.css('#bookTitle::text').extract_first().strip()
+    #authors = response.css.css("a.authorName>span ::text").extract_first().strip()
+    #description = response.css.css("#description span ::text").extract()
+    review_block = response.css(".review")
+    for review in review_block:
+      reviews = review.css(".readable span ::text").extract()
+      
+    
+      items['reviews'] = reviews
 
-      items['titles'] = titles
-      items['authors'] = authors
-      items['tags'] = tags
-
+    #items['titles'] = title
+    #items['authors'] = authors
+    #items['description'] = description
+    
       yield items
+    
+    next_page = 'https://www.goodreads.com/book/show/10210.Jane_Eyre?page='+ str(QuoteSpider.page_number) 
+    if QuoteSpider.page_number<=5:
+      QuoteSpider.page_number+=1
+      
+      yield response.follow(next_page, callback=self.parse)

14c0f45e1dcf93c0a872b178e054c1f69d185f72
asmakrl <90398895+asmakrl@users.noreply.github.com>
2022-06-05 23:43:09 +0100

    done with the optimisation of the file.... All the way to the generalization of the extraction

--- scraping_goodreads/scraping_goodreads/items.py
+++ scraping_goodreads/scraping_goodreads/items.py
@@ -3,14 +3,25 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+from optparse import IndentedHelpFormatter
 import scrapy
 
 
 class ScrapingGoodreadsItem(scrapy.Item):
     # define the fields for your item here like:
     titles = scrapy.Field()
+    
     authors = scrapy.Field()
+
     #description = scrapy.Field()
     reviews = scrapy.Field()
+    review = scrapy.Field()
+
     global_rating = scrapy.Field()
+
     ratings = scrapy.Field()
+    rating = scrapy.Field()
+
+    Informations = scrapy.Field()
+    reviews_dict = scrapy.Field()
+    Books = scrapy.Field()
--- scraping_goodreads/scraping_goodreads/spiders/spider_goodreads.py
+++ scraping_goodreads/scraping_goodreads/spiders/spider_goodreads.py
@@ -1,5 +1,6 @@
 import scrapy
 from ..items import ScrapingGoodreadsItem
+import requests
 
 
 class GoodreadsSpider( scrapy.Spider ):
@@ -14,26 +15,32 @@ class GoodreadsSpider( scrapy.Spider ):
     #'https://www.goodreads.com/genres/list'
 
     ]
+    
+  reviews_ratings = []
+
+  informations = ScrapingGoodreadsItem()
+
 
   # First parse method
   def parse( self, response ):
     
     items = ScrapingGoodreadsItem()
     
+    if (GoodreadsSpider.page_number == 2):
+      title = response.css('#bookTitle::text').extract_first().strip()
+      GoodreadsSpider.informations['titles'] = title
 
-    title = response.css('#bookTitle::text').extract_first().strip()
-    items['titles'] = title
+      authors = response.css("a.authorName>span ::text").extract_first().strip()
+      GoodreadsSpider.informations['authors'] = authors
 
-    authors = response.css("a.authorName>span ::text").extract_first().strip()
-    items['authors'] = authors
-
-    global_rating = response.css("#bookMeta span ::text").extract_first().strip()
-    items['global_rating'] = global_rating
+      global_rating = response.css("#bookMeta span ::text").extract_first().strip()
+      GoodreadsSpider.informations['global_rating'] = global_rating
 
     
     #description = response.css.css("#description span ::text").extract()
     
-    
+    reviews_dict = {}
+
     review_block = response.css(".review")
     
     #if review_block.css(".readable:not(a)"):
@@ -54,22 +61,17 @@ class GoodreadsSpider( scrapy.Spider ):
 
         if review.css(".readable span[id^='freeTextContainer'] ") and review.css("span:not(blockquote)"):
           reviews =  review.css("span[id^='freeText'] ::text").extract()
-        
-
-      items['reviews'] =' '.join([str(item).strip() for item in reviews])
+      items['review'] =' '.join([str(item).strip() for item in reviews])
       
       ratings = 0
       
       for span in review.css("span.p10"):
-         ratings+=1
-         
-      items['ratings'] = ratings
-    
-    
-    #items['description'] = description
-    
-      yield items
-    
+         ratings+=1  
+      items['rating'] = ratings
+
+      reviews_dict = {'reviews': items['review'] , 'ratings': items['rating']}
+      GoodreadsSpider.reviews_ratings.append(reviews_dict)
+
     next_page = 'https://www.goodreads.com/book/show/10210.Jane_Eyre?page='+ str(GoodreadsSpider.page_number) 
    
     if response:
@@ -77,3 +79,11 @@ class GoodreadsSpider( scrapy.Spider ):
       GoodreadsSpider.page_number+=1
       
       yield response.follow(next_page, callback=self.parse)
+    
+    request = requests.head(next_page)
+    code_status = request.status_code
+    website_isup = code_status == 400
+    print(website_isup)
+    if website_isup:
+      GoodreadsSpider.informations['reviews'] = GoodreadsSpider.reviews_ratings
+      yield {'Books' : GoodreadsSpider.informations}
